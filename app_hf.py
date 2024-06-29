82ae44f536bf90372da7003753bfbba5d0eaf6eb
Analia Pastrana <71148492+Analia2020@users.noreply.github.com>
2024-06-29 10:10:40 -0300

    Add files via upload

---
+++ app_hf.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+
+st.title("Clasificador de ecografias mamarias")
+
+# Embeber la aplicación Gradio usando un iframe
+#st.write("Aquí está la aplicación Gradio:")
+st.write('<iframe src="https://nancy1906-equipo-6-proyecto-breast-cancer.hf.space" width="800" height="600"></iframe>', unsafe_allow_html=True)
