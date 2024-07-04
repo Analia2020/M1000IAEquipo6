715424cb532b71c96abba3aeee86050de99aedfa
Analia2020 <analia.pastrana@gmail.com>
2024-07-03 22:59:30 -0300

    Update app_hf.py

--- app_hf.py
+++ app_hf.py
@@ -93,7 +93,7 @@ Este proyecto fue desarrollado dentro del programa **Mil Mujeres en IA**
 def quienes_somos():
     st.title("Quiénes Somos")
     st.image("Home.png", width=700) 
-    st.write("Unidas por el programa **M1000IA**, conformamos el **equipo 6**, un grupo diverso de mujeres de **Argentina, Costa Rica y Colombia.** Nuestras diferentes formaciones se complementan, creando una sinergia que ha sido fundamental para el éxito de este proyecto.")
+    st.write("Unidas por el programa **M1000IA**, conformamos el **equipo 6**, un grupo diverso de mujeres de **Argentina 🇦🇷, Costa Rica y Colombia.** Nuestras diferentes formaciones se complementan, creando una sinergia que ha sido fundamental para el éxito de este proyecto.")
     expander = st.expander("Repositorio  📦")
     expander.write("""Estos son nuestros repositorios:
                    
